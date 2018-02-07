c11c412269d60105676451dd2cc13a40a479c7e0
FravashY <fravashy@gmail.com>
2018-02-08 01:54:50 +0300

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 let mongoose = require('mongoose');
 
 const REMOTE_MONGO = 'mongodb://admin:admin@ds012058.mlab.com:12058/node-todo-app';
-const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
+const LOCAL_MONGO = process.env.MONGO_URI;//'mongodb://localhost:27017/TodoApp';
 const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+console.log('MONGO_URI: ', MONGO_URI);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI).then(() => {
